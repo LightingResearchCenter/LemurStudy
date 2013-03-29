f4b2be748c558be944246a80c6b8509a54d4de0a
Geoffrey Jones <jonesg5@rpi.edu>
2013-03-29 11:39:20 -0400

    Finished skeleton of speciesMillerPlot

---
+++ MillerPlot.m
@@ -0,0 +1,93 @@
+function MillerPlot(TI, AI, CS, days, Title)
+%MILLERPLOT Creates overlapping area plots of activity index and circadian
+%stimulus for a 24 hour period.
+%   time is in MATLAB date time series format
+%   AI is activity index from a Daysimeter or Dimesimeter
+%   CS is Circadian Stimulus from a Daysimeter or Dimesimeter
+%   days is number of days experiment ran for
+
+% Trim data to length of experiment
+delta = find(TI <= days); % indices of data to keep
+TI = TI(delta);
+AI = AI(delta);
+CS = CS(delta);
+
+% Reshape data into columns of full days
+% ASSUMES CONSTANT SAMPLING RATE
+dayIdx = find(TI >= 1,1,'first') - 1;
+extra = rem(length(TI),dayIdx)-1;
+CS(end-extra:end) = [];
+AI(end-extra:end) = [];
+CS = reshape(CS,dayIdx,[]);
+AI = reshape(AI,dayIdx,[]);
+
+% Average data across days
+mCS = mean(CS,2);
+mAI = mean(AI,2);
+
+% Trim time index
+TI = TI(1:dayIdx);
+% Convert time index into hours from start
+hour = TI*24;
+
+% Create area plots
+% Create figure
+figure1 = figure;
+set(figure1,'PaperUnits','inches',...
+    'PaperType','usletter',...
+    'PaperOrientation','landscape',...
+    'PaperPositionMode','manual',...
+    'PaperPosition',[0.5 0.5 10 7.5],...
+    'Units','inches',...
+    'Position',[0 0 11 8.5]);
+
+% Create axes
+axes1 = axes('Parent',figure1,'XTick',0:2:24,...
+    'TickDir','out',...
+    'DataAspectRatio',[4 1 1]);
+xlim(axes1,[0 24]);
+% Ymax = max([max(mAI),max(mCS)]);
+% Ymax = ceil(Ymax/0.5)*0.5;
+Ymax = 1;
+ylim(axes1,[0 Ymax]);
+box(axes1,'off');
+hold(axes1,'all');
+
+% Create custom grid
+YgridPos = 0.25:0.25:(Ymax-.25);
+XgridPos = 2:2:22;
+GridColor = [0.8 0.8 0.8];
+% Y grid lines
+for i1 = 1:length(YgridPos)
+    line([0 24],[YgridPos(i1),YgridPos(i1)],'Color',GridColor,'LineStyle',':');
+end
+% X grid lines
+for i1 = 1:length(XgridPos)
+    line([XgridPos(i1),XgridPos(i1)],[0 Ymax],'Color',GridColor,'LineStyle',':');
+end
+
+% Create areas
+area1 = area(hour,[mAI,mCS],'LineStyle','none');
+set(area1(1),...
+    'FaceColor',[0.6 0.6 0.6],'EdgeColor','none',...
+    'DisplayName','CS - Circadian Stimulus');
+set(area1(2),...
+    'FaceColor',[0.2 0.2 0.2],'EdgeColor','none',...
+    'DisplayName','AI - Activity Index');
+
+% Create legend
+legend1 = legend(area1);
+set(legend1,'Orientation','horizontal','Location','NorthOutside');
+
+% Create x-axis label
+xlabel('hour');
+
+% Create title
+title(Title);
+
+% Close box around plot
+line([0 24],[Ymax Ymax],'Color','k');
+line([24 24],[0 Ymax],'Color','k');
+
+end
+
--- speciesMillerPlot.m
+++ speciesMillerPlot.m
@@ -1,47 +1,119 @@
 function speciesMillerPlot
 %SPECIESMILLERPLOT Creates Miller Plots of data averaged across a species
 
-load('lemurData.mat','ID','gender','species','subject','time','CS','AI');
+load('lemurData.mat','species','subject','time','CS','AI');
 
 unqSpecies = unique(species);
 n = length(unqSpecies);
 
-% Preallocate variables
 spcIdx = cell(n,1); % indicies of subjects that belong to each species
+timeIndex = cell(n,1);
+CS1 = cell(n,1);
+AI1 = cell(n,1);
+close all;
 for i1 = 1:n
     spcIdx{i1} = find(strcmp(unqSpecies{i1},species) == 1);
-    averageSpecies(time(spcIdx{i1}),CS(spcIdx{i1}),AI(spcIdx{i1}));
+    % Process data
+    [timeIndex{i1},CS1{i1},AI1{i1}] = averageSpecies(time(spcIdx{i1}),...
+        CS(spcIdx{i1}),AI(spcIdx{i1}));
+    % Generate figure and save to disk
+    MillerPlot(timeIndex{i1},AI1{i1},CS1{i1},floor(timeIndex{i1}(end)),...
+        unqSpecies{i1});
+    print(gcf,'-dpdf',fullfile('speciesMillerPlots',[unqSpecies{i1},...
+        '.pdf']));
+    close;
 end
 
 end
 
-function averageSpecies(time0,CS0,AI0)
+function [timeIndex,CS,AI] = averageSpecies(time0,CS0,AI0)
 %AVERAGESPECIES Average data from all subjects that belong to one species
 
-[leastDays,timeIndex] = shortestTime(time0);
+% Pre-process input data
+[timeIndex1,CS1,AI1] = shortestTime(time0,CS0,AI0);
+timeIndex2 = matchSampling(timeIndex1);
+CS2 = matchSampling(CS1);
+AI2 = matchSampling(AI1);
+
+% Average data
+timeIndex = timeIndex2{1};
+CS3 = cell2mat(CS2);
+AI3 = cell2mat(AI2);
+CS = mean(CS3,2);
+AI = mean(AI3,2);
 
 end
 
-function [leastDays,timeIndex] = shortestTime(times)
-%SHORTESTTIME Finds the least number of days in multiple arrays of dates
-%   times is a cell array containing date arrays
-%   returns time
+function [timeIndex,CS2,AI2] = shortestTime(time,CS,AI)
+%SHORTESTTIME Matches data time scale to shortest set
+%   time is a cell array containing date arrays
+%   returns time indexs relative to first midnight
+%   function is independent of data sampling rates
 
-n = length(times);
+n = length(time);
 runTime = zeros(n,1);
 timeIndex0 = cell(n,1);
+timeIndex1 = cell(n,1);
+CS1 = cell(n,1);
+AI1 = cell(n,1);
 % Find run time in whole days
-for i1 = 1:n
-    timeIndex0{i1} = times{i1} - times{i1}(1);
-    runTime(i1) = floor(timeIndex0{i1}(end));
+for i2 = 1:n
+    timeIndex0{i2} = time{i2} - ceil(time{i2}(1));
+    idx1 = timeIndex0{i2} >= 0;
+    timeIndex1{i2} = timeIndex0{i2}(idx1);
+    runTime(i2) = floor(timeIndex1{i2}(end));
+    
+    % Trim CS and AI
+    CS1{i2} = CS{i2}(idx1);
+    AI1{i2} = AI{i2}(idx1);
 end
 
 leastDays = min(runTime);
 
+% Trim time data to length of shortest
 timeIndex = cell(n,1);
+CS2 = cell(n,1);
+AI2 = cell(n,1);
+for i2 = 1:n
+    idx2 = timeIndex1{i2} <= leastDays;
+    timeIndex{i2} = timeIndex1{i2}(idx2);
+    CS2{i2} = CS1{i2}(idx2);
+    AI2{i2} = AI1{i2}(idx2);
+end
+
+end
+
+function data = matchSampling(data0)
+%MATCHSAMPLING Matches data sampling rates using decimation
+%   assumes data sets have already been shortened to the same time scale
+%   data0 must be cell array of double arrays
+
+% Find the population size of each data set
+n = length(data0);
+population = zeros(n,1);
 for i1 = 1:n
-    idx = timeIndex0{i1} <= leastDays;
-    timeIndex{i1} = timeIndex0{i1}(idx);
+    population(i1) = length(data0{i1});
+end
+
+minPop = min(population);
+maxPop = max(population);
+
+maxR = max(floor(population/minPop));
+
+% Match sampling data rate of all sets to least populated
+data = cell(n,1);
+if max(maxR) > 1
+    minPop = maxPop/maxR;
+end
+r = population/minPop;
+for i2 = 1:n
+    if r(i2) == 1
+        data{i2} = data0{i2};
+    elseif 1 < r(i2) < 2
+        data{i2} = data0{i2}(1:minPop);
+    else
+        data{i2} = decimate(data0{i2},r(i2));
+    end
 end
 
 end
