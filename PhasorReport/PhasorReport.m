45b0f725c088795237308f54b178e504631cd9ed
Geoffrey Jones <jonesg5@rpi.edu>
2013-04-15 17:20:54 -0400

    Added light dark to Phasor Report

--- PhasorReport/PhasorReport.m
+++ PhasorReport/PhasorReport.m
@@ -1,15 +1,14 @@
-function PhasorReport(stime,sCS,sAI,time,CS,AI,Title)
+function PhasorReport(stime,sCS,sAI,time,CS,AI,Title,dateRange)
 %PHASORREPORT Generates graphical summary of CS and Activity
 
-% Process and analyze data
+%% Process and analyze data
 % Trim data to length of experiment
-TI = stime - stime(1); % time index in days from start
-days = floor(max(TI));
-delta = find(TI <= days); % indices of data to keep
-stime = stime(delta);
-TI = TI(delta);
-sAI = sAI(delta);
-sCS = sCS(delta);
+idxDate = stime >= dateRange(1) & stime <= dateRange(2)+1;
+stime = stime(idxDate);
+TI = stime - dateRange(1);
+days = floor(TI(end));
+sAI = sAI(idxDate);
+sCS = sCS(idxDate);
 
 epoch = ((stime(2)-stime(1))*(24*3600)); % sample epoch in seconds
 Srate = 1/epoch; % sample rate in Hertz
@@ -31,9 +30,32 @@ for i1 = 1:numSubjects
     [phasorMagnitude(i1), phasorAngle(i1)] = cos24(CS{i1}, AI{i1}, time{i1});
 end
 
-meanMagnitude = mean(phasorMagnitude);
-meanAngle = mean(phasorAngle);
+% Calculate light and dark related information
+load(fullfile('astronomicalData','sunrise_set.mat'),'sunRise','sunSet');
+% Find sunrise and sunset times that cover the date range
+idxRise = sunRise >= dateRange(1)-1 & sunRise <= dateRange(2)+1;
+idxSet = sunSet >= dateRange(1)-1 & sunSet <= dateRange(2)+1;
+idxRiseSet = idxRise | idxSet;
+sunRise = sunRise(idxRiseSet);
+sunSet = sunSet(idxRiseSet);
 
+idxDay = zeros(length(stime),1);
+idxNight = zeros(length(stime),1);
+for i1 = 1:length(sunRise)-1
+    idxDayTemp = stime >= sunRise(i1) & stime < sunSet(i1);
+    idxDay = idxDay | idxDayTemp;
+    idxNightTemp = stime >= sunSet(i1) & stime < sunRise(i1+1);
+    idxNight = idxNight | idxNightTemp;
+end
+totalAI = sum(sAI);
+dayAI = sum(sAI(idxDay));
+nightAI = sum(sAI(idxNight));
+perDayAI = dayAI*100/totalAI;
+perNightAI = nightAI*100/totalAI;
+ratioDayNightAI = dayAI/nightAI;
+
+
+%% Create graphics
 % Create figure
 figure1 = figure;
 paperPosition = [0 0 11 8.5];
@@ -82,7 +104,7 @@ set(dateHandle,'Position',datePosition);
 workHeight = titlePosition(2)-ySpace-yMargin;
 workWidth = 1-2*xMargin;
 
-% Panel 1 Activity and CS
+%% Panel 1 Activity and CS
 % Set position values
 x1 = xMargin; % half an inch from the edge
 w1 = 1-2*xMargin; % width fot half inch margins
@@ -92,44 +114,58 @@ y1 = workHeight+yMargin-h1;
 axes1 = axes('Parent',figure1,'OuterPosition',[x1 y1 w1 h1]);
 hold(axes1);
 set(axes1,'Box','off','TickDir','out');
+ymax = ceil(max([max(sAI),max(sCS)])/.5)*.5;
+area2 = area(axes1,TI,idxNight*ymax);
+set(area2,'FaceColor','b','EdgeColor','none','DisplayName','Night');
+alpha(.5);
 area1 = area(axes1,TI,sAI);
 set(area1,'FaceColor',[.2 .2 .2],'EdgeColor','none','DisplayName','Activity');
 plot1 = plot(axes1,TI,sCS);
-set(plot1,'Color',[.6 .6 .6],'LineWidth',2,'DisplayName','Circadian Stimulus');
+set(plot1,'Color',[.6 .6 .6],'LineWidth',1.5,'DisplayName','Circadian Stimulus');
 xlim(axes1,[0 days]);
 set(axes1,'xtick',0:1:days);
-ymax = ceil(max([max(sAI),max(sCS)])/.5)*.5;
 ylim(axes1,[0 ymax]);
-legend(axes1,'Location','NorthOutside','Orientation','horizontal');
+legend([area1,plot1,area2],'Location','NorthOutside','Orientation','horizontal');
 
-% Panel 2 Phasors
+%% Panel 2 Phasors
 h2 = 2*workHeight/3-ySpace/2;
 y2 = yMargin;
 w2 = h1*paperPosition(3)/paperPosition(4);
 x2 = x1+w2/4;
 %   Plot phasors
 axes('Parent',figure1,'OuterPosition',[x2 y2 w2 h2]);
-phasorplot(phasorMagnitude,phasorAngle,.75,3,6,'top','left',.1);
-title(gca,'CS/Activity Phasor');
-hold(gca);
-phasorplot(sMagnitude,sAngle,.75,3,6,'top','left',.1,0,0,'r');
+hc1 = phasorplot(phasorMagnitude,phasorAngle,.75,3,6,'top','left',.1);
+axes2 = gca;
+title(axes2,'CS/Activity Phasor');
+hold(axes2);
+hc2 = phasorplot(sMagnitude,sAngle,.75,3,6,'top','left',.1,0,0,'r');
+group1 = hggroup;
+group2 = hggroup;
+set(hc1,'Parent',group1);
+set(hc2,'Parent',group2);
+legend2 = legend([group1,group2],'Individuals','Species');
+set(legend2,'Location','SouthOutside','Orientation','horizontal');
 
-% Panel 3 Text annotations
+%% Panel 3 Text annotations
 h3 = h2;
 y3 = y2;
-w3 = (workWidth-xSpace)/2;
+w3 = workWidth/2+xSpace;
 x3 = xMargin+w3;
-notes = cell(9,1);
-notes{1} = ['Phasor Magnitude: ', num2str(sMagnitude, '%.2f')];
-notes{2} = ['Phasor Angle: ', num2str(sAngle, '%.2f'), ' hours'];
+notes = cell(13,1);
+notes{1} = ['Phasor Magnitude: ', num2str(sMagnitude,'%.2f')];
+notes{2} = ['Phasor Angle: ', num2str(sAngle,'%.2f'),' hours'];
 notes{3} = ' ';
-notes{4} = ['IS: ', num2str(IS, '%.2f')];
-notes{5} = ['IV: ', num2str(IV, '%.2f')];
+notes{4} = ['IS: ', num2str(IS,'%.2f')];
+notes{5} = ['IV: ', num2str(IV,'%.2f')];
 notes{6} = ' ';
-notes{7} = ['Average CS: ', num2str(mean(sCS), '%.2f')];
+notes{7} = ['Average CS: ', num2str(mean(sCS),'%.2f')];
 notes{8} = ['Mag w/ harmonics: ' num2str(MagH,'%.3f')];
 notes{9} = ['Mag 1st harmonic: ' num2str(abs(f24),'%.3f')];
+notes{10} = ' ';
+notes{11} = ['Daytime Activity: ',num2str(perDayAI,'%.2f'),'%'];
+notes{12} = ['Nighttime Activity: ',num2str(perNightAI,'%.2f'),'%'];
+notes{13} = ['Day/Night Acyivity Ratio: ',num2str(ratioDayNightAI,'%.3f')];
 text3 = annotation(figure1,'textbox', [x3 y3 w3 h3], 'String',notes);
 set(text3,'EdgeColor','none','HorizontalAlignment','left',...
-    'VerticalAlignment','middle','FontSize',14);
+    'VerticalAlignment','middle','FontSize',11);
 end
--- speciesPhasorReport.m
+++ speciesPhasorReport.m
@@ -8,7 +8,8 @@ n = length(unqSpecies);
 close all;
 for i1 = 1:n
     idx = spcIdx{i1};
-    PhasorReport(timeIndex{i1},CS1{i1},AI1{i1},time(idx),CS(idx),AI(idx),unqSpecies{i1})
+    Title = {unqSpecies{i1};[datestr(dateRange(i1,1),'mm/dd/yyyy'),' - ',datestr(dateRange(i1,2),'mm/dd/yyyy')]};
+    PhasorReport(timeIndex{i1}+dateRange(i1,1),CS1{i1},AI1{i1},time(idx),CS(idx),AI(idx),Title,dateRange(i1,:));
     % Save to disk
     fileName = fullfile('phasorReports',unqSpecies{i1});
     print(gcf,'-dpdf',[fileName,'.pdf'],'-r200');
