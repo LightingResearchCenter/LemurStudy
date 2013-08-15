a24f737d7d42d003635be91f6201c23506b817ac
Geoffrey Jones <jonesg5@rpi.edu>
2013-08-15 11:17:04 -0400

    Fixed Izze

--- august2013PhasorReport.m
+++ august2013PhasorReport.m
@@ -1,4 +1,4 @@
-function august2013PhasorReport
+function August2013PhasorReport
 %SPECIESPHASORREPORT Generate Phasor Report for each species
 %   Requires PhasorReport package
 addpath('PhasorReport');
@@ -7,7 +7,7 @@ load('2013AugustLemurData.mat');
 n = length(dimeSN);
 close all;
 for i1 = 1:n
-    Title = {['Name: ',name{i1},', Gender: ',gender{i1},', Age [years]: ',num2str(ageYears(i1))];...
+    Title = {['Name: ',name{i1},', Gender: ',gender{i1},', Age: ',num2str(ageYears(i1)),' years'];...
         [datestr(time{i1}(1),'mm/dd/yyyy HH:MM'),' - ',datestr(time{i1}(end),'mm/dd/yyyy HH:MM')]};
     PhasorReport(time{i1},CS{i1},AI{i1},Title);
     % Save to disk
